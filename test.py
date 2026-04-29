e9a146c2f4cad7c65e2a62d41345954cd455770b
Andrew Han <150089276+Arata-Kokonoe@users.noreply.github.com>
2026-04-29 17:29:24 +0000

    testing

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 from draughts import Board, Move, WHITE, BLACK
 #Create a game
-board = Board(variant="standard", fen="startpos")
+board = Board(variant="english", fen="startpos")
+
 #Make a move
-move = Move(board, steps_move=[34, 30])
-board.push(move)
+#move = Move(board, steps_move=[34, 30])
+#board.push(move)
 
 # Multi-capture
 board2 = Board(fen="W:WK40:B19,29")
@@ -15,3 +16,6 @@ svg.create_svg(Board(fen="B:W16,19,33,34,47,K4:B17,25,26"))
 
 #Get a visual representation of the board in the terminal
 print(board)
+
+moves = board.legal_moves()
+print(moves)

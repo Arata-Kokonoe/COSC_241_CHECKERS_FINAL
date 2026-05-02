eecb819ada4c3a8b407056c11b988282e901316c
Andrew Han <150089276+Arata-Kokonoe@users.noreply.github.com>
2026-05-02 19:22:01 +0000

    added blueprint MCTS method and Node class

--- mcts.py
+++ mcts.py
@@ -1,10 +1,11 @@
 import math, random
+from node import Node
 from draughts import Board, Move, WHITE, BLACK
 from copy import deepcopy
 
-starting_board = Board(variant="english", fen="startpos")
+#starting_board = Board(variant="english", fen="startpos")
 
-BOARD_SIZE = starting_board.width
+#BOARD_SIZE = starting_board.width
 
 #returns 1 if white won, 2 if black won, 0 if none
 def check_winner_state(board):
@@ -14,3 +15,19 @@ def check_winner_state(board):
 
 def available_actions(board):
     return board.legal_moves()
+
+def MCTS(board, numIterations, explorationParameter, simIterations):
+    root = Node(board)
+    nextStates = []
+    for move in board.legalMoves():
+        newBoard = board.copy()
+        newBoard.push(move)
+        nextStates.append(newBoard)
+    for state in nextStates: 
+        root.children.add(Node(nextStates))
+    for i in [0, numIterations]:
+        # 1: Selection
+        # 2: 
+        # 3: Simulate
+        # 4: Backpropagate
+        break
--- node.py
+++ node.py
@@ -1,7 +1,15 @@
 class Node:
-    def __init__(self, state, parent, children, move):
-        self.state = state
-        self.parent = parent
-        self.children = children
+    
+    #def __init__(self, state, parent, children, move):
+    #    self.state = state
+    #    self.parent = parent
+    #    self.children = children
+    #    self.numVisits = 0
+    #    self.value = 0
+
+    def __init__(self, board):
+        self.state = board
+        #self.parent = parent
+        #self.children = children
         self.numVisits = 0
         self.value = 0

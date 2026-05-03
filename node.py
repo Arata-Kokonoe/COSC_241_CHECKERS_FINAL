dcae53263254374001e8d036460bd8d0061a0894
bshi27 <bryan7shi@gmail.com>
2026-05-02 23:03:21 -0400

    fixing some parts of mcts and some testing

--- node.py
+++ node.py
@@ -11,11 +11,11 @@ class Node:
     #    self.numVisits = 0
     #    self.value = 0
 
-    def __init__(self, board, parent = None, children = None, move = None, numVisits = 0, value = 0, untriedMoves = []):
+    def __init__(self, board, parent = None, children = [], move = None, numVisits = 0, value = 0, untriedMoves = []):
         self.state = board
         self.parent = parent
         self.children = children
         self.move = move
         self.numVisits = numVisits
         self.value = value
-        self.untriedMoves = board.legalMoves()
+        self.untriedMoves = board.legal_moves()

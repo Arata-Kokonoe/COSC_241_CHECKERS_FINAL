b306a33741100e7e02d278890c23b5a94de4dc19
bshi27 <bryan7shi@gmail.com>
2026-05-02 21:55:01 -0400

    built framework for mcts and small changes to node

--- mcts.py
+++ mcts.py
@@ -18,15 +18,60 @@ def available_actions(board):
 
 def MCTS(board, numIterations, explorationParameter, simIterations):
     root = Node(board)
-    nextStates = []
-    for move in board.legalMoves():
-        newBoard = board.create_new_board_from_move(move)
-        nextStates.append(newBoard)
-    for state in nextStates: 
-        root.children.add(Node(nextStates))
-    for i in [0, numIterations]:
+    # for move in board.legalMoves():
+    #     newBoard = board.create_new_board_from_move(move)
+    #     nextStates.append(newBoard)
+    # for state in nextStates: 
+    #     root.children.add(Node(nextStates))
+    for i in range(numIterations):
         # 1: Selection
-        # 2: 
+        nodeToExpand = root
+        while (not nodeToExpand.state.is_over()) and (len(nodeToExpand.untriedMoves) == 0):
+            nodeToExpand = getBestChild(nodeToExpand, explorationParameter)
+        # 2: Expansion
+        move = nodeToExpand.untriedMoves.pop()
+        newBoard = nodeToExpand.state.create_new_board_from_move(move)
+        nodeToEvaluate = Node(newBoard, nodeToExpand, None, move)
+        nodeToExpand.children.add(nodeToEvaluate)
         # 3: Simulate
+        value = simulation(nodeToEvaluate, simIterations)
         # 4: Backpropagate
-        break
+        nodeToEvaluate.value = value
+        nodeToEvaluate.numVisits += 1
+        currNode = nodeToEvaluate
+        while currNode.parent:
+            currNode = nodeToEvaluate.parent
+            currNode.value = value
+            currNode.numVisits += 1
+    # get the best action from root after numIterations
+    bestChild = max(root.children, key=lambda child: child.value)
+    return bestChild.move
+
+def getBestChild(node, explorationParameter):
+    for child in node.children:
+        if child.numVisits == 0: return child
+    childrenValues = {}
+    for child in node.children:
+        childrenValues[child] = UCB(child, explorationParameter)
+    return max(childrenValues, key=childrenValues.get)
+
+def UCB(node, explorationParameter):
+    UCB = node.value + (explorationParameter * math.sqrt(math.log(node.parent.numVisits) / node.numVisits))
+    return UCB
+
+def fastRollout(board):
+    currState = board
+    while not currState.is_over():
+        moves = currState.legal_moves()
+        nextMove = random.choice(moves)	# should be changed maybe? Use heuristic policy instead of random
+        currState = currState.create_new_board_from_move(nextMove)
+    # CHANGE CHANGE CHANGE (how do we tell the agent/opponent is playing for black or white?)
+    if currState.winner() == WHITE: return 1
+    elif currState.winner() == BLACK: return -1
+    else: return 0
+
+def simulation(node, simIterations):
+    totalValue = 0
+    for i in range(simIterations):
+        totalValue += fastRollout(node.state)
+    return totalValue / simIterations
--- node.py
+++ node.py
@@ -1,3 +1,7 @@
+import math, random
+from draughts import Board, Move, WHITE, BLACK
+from copy import deepcopy
+
 class Node:
     
     #def __init__(self, state, parent, children, move):
@@ -7,9 +11,11 @@ class Node:
     #    self.numVisits = 0
     #    self.value = 0
 
-    def __init__(self, board, parent = None, children = None):
+    def __init__(self, board, parent = None, children = None, move = None, numVisits = 0, value = 0, untriedMoves = []):
         self.state = board
         self.parent = parent
         self.children = children
-        self.numVisits = 0
-        self.value = 0
+        self.move = move
+        self.numVisits = numVisits
+        self.value = value
+        self.untriedMoves = board.legalMoves()

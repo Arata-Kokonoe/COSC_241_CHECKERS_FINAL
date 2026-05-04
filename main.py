ec04b82f6c0ff26eb26aae3f68471d78a5a7b5c6
bshi27 <bryan7shi@gmail.com>
2026-05-04 13:28:50 -0400

    parallel simulations

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
+from time import time
+
 from draughts import Board, svg
 from mcts import MCTS
 from mctsClass import mctsClass
 
-board = Board(variant="english", fen="startpos")
-move_count = 0
-mcts = mctsClass(board)
-while not board.is_over():
-    #tests mcts function version (original)
-    #move = MCTS(board, numIterations=10, explorationParameter=1.4, simIterations=5)
-    
-    #tests mcts class version
-    move = mcts.search(numIterations=10, explorationParameter=1.4, simIterations=5)
+def main():
+    board = Board(variant="english", fen="startpos")
+    move_count = 0
+    mcts = mctsClass(board)
+    while not board.is_over():
+        #tests mcts function version (original)
+        t1 = time()
+        move = MCTS(board, numIterations=30, explorationParameter=1.4, simIterations=50)
+        t2 = time()
+        print(f"MCTS Move Time: {t2-t1:.2f}s")
+        #tests mcts class version
+        # t1 = time()
+        # move = mcts.search(numIterations=10, explorationParameter=1.4, simIterations=10)
+        # t2 = time()
+        # print(f"MCTS Move Time: {t2-t1:.2f}s")
+        board.push(move)
+        move_count += 1
+        print(f"Move {move_count}: {move.pdn_move}")
+        print(board)  # Display board as text
+    print(f"Game ended. Winner: {board.winner()}")
 
-    #move = mcts.search(numIterations=10, explorationParameter=1.4, simIterations=30)
-    
-    board.push(move)
-    move_count += 1
-    print(f"Move {move_count}: {move.pdn_move}")
-    print(board)  # Display board as text
-print(f"Game ended. Winner: {board.winner()}")
+if __name__ == "__main__":
+    main()

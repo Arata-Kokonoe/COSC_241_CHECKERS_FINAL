c29b41893cdd74d04d16526b370db7f502502ce2
Andrew Han <150089276+Arata-Kokonoe@users.noreply.github.com>
2026-05-04 00:05:30 +0000

    removed useless function

--- mcts.py
+++ mcts.py
@@ -3,12 +3,6 @@ from node import Node
 from draughts import Board, Move, WHITE, BLACK
 from copy import deepcopy
 
-#returns 1 if white won, 2 if black won, 0 if none
-def check_winner_state(board):
-    if board.winner() == WHITE: return 1
-    elif board.winner() == BLACK: return 2
-    else: return None
-
 def MCTS(board, numIterations, explorationParameter, simIterations):
     root = Node(board)
     rootPlayer = board.turn
